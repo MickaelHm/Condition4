38f00fe9c8c1a8ecd3024957aae94888d1d09572
MickaelHm <harmeau.mickael@gmail.com>
2018-12-19 10:10:40 +0100

    Complet

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condition4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String login = "Mickael";
+            String password = "123";
+            Console.WriteLine("Entrez votre identifiant"); ;
+            string userLogin = Console.ReadLine();
+            Console.WriteLine("Entrez votre mot de passe");
+            string userPassword = Console.ReadLine();
+
+            if(login == userLogin)
+            {
+                Console.WriteLine("Bienvenue à la Manu le Havre.");
+            }
+            else
+            {
+                Console.WriteLine("Mauvais identifiant ou mauvais mot de passe.");
+            }
+        }
+    }
+}
